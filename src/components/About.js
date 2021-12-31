5f2a243b3fb3e033e93b28ea2d7ab388cfb3cdac
Jeffrey Kim <64029918+jeffkim1118@users.noreply.github.com>
2021-12-30 20:27:30 -0500

    still working on it

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
 function About(props) {
+  // function isPassed(){
+  //   if(props.bio){
+  //     return <p>{props.bio}</p>
+  //   }
+  // }
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {props.link}
+      <Links links = {props.links}/>
     </div>
   );
 }
